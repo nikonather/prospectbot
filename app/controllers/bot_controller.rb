90c70b7372f7288f9fc7058d5c2d70748b19e364
Niko <niko2@Nikos-MacBook-Air.local>
2016-06-25 22:22:09 +0200

    roles update

--- app/controllers/bot_controller.rb
+++ app/controllers/bot_controller.rb
@@ -44,6 +44,24 @@ class BotController < ApplicationController
     {friendly_name:'CEO', name:'ceo', keywords:['ceo','chief executive officer']},
     {friendly_name:'Human Resources', name:'human_resources', keywords:['hr','human resources']},
     {friendly_name:'Public Relations', name:'public_relations', keywords:['pr','public relations']}
+    {friendly_name:'Communications', name:'communications', keywords:['communications']}
+    {friendly_name:'Consulting', name:'consulting', keywords:['consulting']}
+    {friendly_name:'Customer Service', name:'customer_service', keywords:['customer service']}
+    {friendly_name:'Education', name:'education', keywords:['education']}
+    {friendly_name:'Engineering', name:'engineering', keywords:['engineering']}
+    {friendly_name:'Finance', name:'finance', keywords:['finance']}
+    {friendly_name:'Founder', name:'founder', keywords:['founder','co-founder']}
+    {friendly_name:'Health Professional', name:'health_professional', keywords:['health professional']}
+    {friendly_name:'Information Technology', name:'information_technology', keywords:['it','information technology']}
+    {friendly_name:'Legal', name:'legal', keywords:['legal']}
+    {friendly_name:'Owner', name:'owner', keywords:['owner']}
+    {friendly_name:'President', name:'president', keywords:['president']}
+    {friendly_name:'Product', name:'product', keywords:['product']}
+    {friendly_name:'Real Estate', name:'real_estate', keywords:['real estate']}
+    {friendly_name:'Operations', name:'operations', keywords:['operations']}
+    {friendly_name:'Recruiting', name:'recruiting', keywords:['recruiting']}
+    {friendly_name:'Research', name:'research', keywords:['research']}
+    {friendly_name:'Sales', name:'sales', keywords:['sales']}
     ]
   end
 
